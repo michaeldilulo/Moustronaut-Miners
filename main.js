dc5fd6445e1ef48ee23c81a23c2c0ff73b6c4060
michael dilulo <snoleopard01001101@gmail.com>
2020-10-14 10:59:11 -0700

    Var Separation, formatting, HTML, CSS

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-// DATA
 let cheeseCollected = 0;
-let cheeseKnifePurchased = 10;
-let cheeseGraterPurchased = 25;
-let cheeseKnifeCount = 0;
-let cheeseGraterCount = 0;
 let totalCheeseMine = 0;
 
+let cheeseKnifePurchased = 25;
+let cheeseGraterPurchased = 75;
+
+let cheeseKnifeCount = 0;
+let cheeseGraterCount = 0;
+
+
 let allCheeseCollected = document.getElementById('cheeseCount');
+let totalMineCheese = document.getElementById('cheeseMineTotal')
+
 let cheeseKnifeButton = document.getElementById('cheeseKnifeButton');
 let cheeseGraterButton = document.getElementById('cheeseGraterButton')
+
 let inventoryCheeseKnife = document.getElementById('cheeseKnifeCount');
 let inventoryCheeseGrater = document.getElementById('cheeseGraterCount')
-let totalMineCheese = document.getElementById('cheeseMineTotal')
+
+cheeseKnifeButton.setAttribute('disabled', 'true')
+cheeseGraterButton.setAttribute('disabled', 'true')
 
 const mineCheese = () => {
     setInterval(() => {
@@ -25,9 +32,9 @@ const mineCheese = () => {
 }
 
 const abilityCheeseKnife = () => {
-    if (cheeseCollected < 10) {
+    if (cheeseCollected < 25) {
         cheeseKnifeButton.setAttribute('disabled', 'true')
-    } else if (cheeseCollected >= 10) {
+    } else if (cheeseCollected >= 25) {
         cheeseKnifeButton.style.backgroundColor = '#292b2c';
         cheeseKnifeButton.removeAttribute('disabled')
     }
@@ -41,9 +48,9 @@ const purchaseCheeseKnife = () => {
 }
 
 const abilityCheeseGrater = () => {
-    if (cheeseCollected < 25) {
+    if (cheeseCollected < 75) {
         cheeseGraterButton.setAttribute('disabled', 'true')
-    } else if (cheeseCollected >= 25) {
+    } else if (cheeseCollected >= 75) {
         cheeseGraterButton.style.backgroundColor = '#292b2c';
         cheeseGraterButton.removeAttribute('disabled')
     }

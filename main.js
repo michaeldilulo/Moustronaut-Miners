60f84342a9d433e66e7bffc147f924da971512af
michael dilulo <snoleopard01001101@gmail.com>
2020-10-14 10:30:31 -0700

    Cheese Grater, Refactor Code

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 // DATA
 let cheeseCollected = 0;
 let cheeseKnifePurchased = 10;
+let cheeseGraterPurchased = 25;
 let cheeseKnifeCount = 0;
+let cheeseGraterCount = 0;
 let totalCheeseMine = 0;
 
 let allCheeseCollected = document.getElementById('cheeseCount');
 let cheeseKnifeButton = document.getElementById('cheeseKnifeButton');
+let cheeseGraterButton = document.getElementById('cheeseGraterButton')
 let inventoryCheeseKnife = document.getElementById('cheeseKnifeCount');
+let inventoryCheeseGrater = document.getElementById('cheeseGraterCount')
 let totalMineCheese = document.getElementById('cheeseMineTotal')
 
-const startMining = () => {
+const mineCheese = () => {
+    setInterval(() => {
+        cheeseCollected++;
+        totalCheeseMine++;
+        allCheeseCollected.innerText = (cheeseCollected).toString();
+        totalMineCheese.innerText = (totalCheeseMine).toString();
+        abilityCheeseKnife();
+        abilityCheeseGrater();
+    }, 1000);
+}
+
+const abilityCheeseKnife = () => {
     if (cheeseCollected < 10) {
         cheeseKnifeButton.setAttribute('disabled', 'true')
     } else if (cheeseCollected >= 10) {
@@ -18,20 +33,25 @@ const startMining = () => {
     }
 }
 
-const mineCheese = () => {
-    startMining();
-    cheeseCollected++;
-    totalCheeseMine++;
-    allCheeseCollected.innerText = (cheeseCollected).toString();
-    totalMineCheese.innerText = (totalCheeseMine).toString();
+const purchaseCheeseKnife = () => {
+    cheeseKnifeCount++;
+    cheeseCollected - cheeseKnifePurchased;
+    inventoryCheeseKnife.innerText = (cheeseKnifeCount).toString();
+    allCheeseCollected.innerText = (cheeseCollected = cheeseCollected - cheeseKnifePurchased).toString();
 }
 
-const purchaseCheeseKnife = () => {
-    if (cheeseCollected >= 10) {
-        cheeseKnifeCount++;
-        cheeseCollected - cheeseKnifePurchased;
-        inventoryCheeseKnife.innerText = (cheeseKnifeCount).toString();
-        allCheeseCollected.innerText = (cheeseCollected = cheeseCollected - cheeseKnifePurchased).toString();
-        startMining();
+const abilityCheeseGrater = () => {
+    if (cheeseCollected < 25) {
+        cheeseGraterButton.setAttribute('disabled', 'true')
+    } else if (cheeseCollected >= 25) {
+        cheeseGraterButton.style.backgroundColor = '#292b2c';
+        cheeseGraterButton.removeAttribute('disabled')
     }
+}
+
+const purchaseCheeseGrater = () => {
+    cheeseGraterCount++;
+    cheeseCollected - cheeseGraterPurchased;
+    inventoryCheeseGrater.innerText = (cheeseGraterCount).toString();
+    allCheeseCollected.innerText = (cheeseCollected = cheeseCollected - cheeseGraterPurchased).toString();
 }
